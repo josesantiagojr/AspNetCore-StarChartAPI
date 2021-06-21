85d4dbec8d454e8840cff41641a40856b05d4803
Jose Santiago <github@josesantiago.net>
2021-06-21 11:16:26 -0500

    Create CelestialObjectController Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -18,7 +19,49 @@ namespace StarChart.Controllers
             _context = context;
         }
 
+        [HttpGet("{id:int}", Name = "GetById")]       
+        public IActionResult GetById(int id)
+        {
+           var myobject = _context.CelestialObjects.Find(id);
 
+            if(myobject is null)
+            {
+                return NotFound();
+            }
 
+            myobject.Satellites = ((CelestialObject)_context.CelestialObjects.Select(x => x.OrbitedObjectId == myobject.OrbitedObjectId)).Satellites;
+                         
+            return Ok(myobject);
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public ActionResult<CelestialObject[]> GetByName(string name)
+        {
+            var myobjects = _context.CelestialObjects.Find(name);
+
+            if (myobjects is null)
+            {
+                return NotFound();
+            }
+
+            myobjects.Satellites = ((CelestialObject)_context.CelestialObjects.Select(x => x.OrbitedObjectId == myobjects.OrbitedObjectId && x.Name == myobjects.Name)).Satellites;
+
+            return Ok(myobjects);
+        }
+
+        [HttpGet(Name = "GetAll")]
+        public ActionResult<CelestialObject[]> GetAll()
+        {
+            var myobjects = from o in _context.CelestialObjects
+                            select o;
+                            
+
+            if (myobjects.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(myobjects);
+        }
     }
 }
